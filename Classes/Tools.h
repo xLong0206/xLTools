a59c5052cef2c577481be8f15b0ddefff476ca1f
xLonG <295945232@qq.com>
2019-03-20 09:34:43 +0800

    提交工具类

---
+++ Classes/Tools.h
@@ -0,0 +1,14 @@
+//
+//  Tools.h
+//  Tools
+//
+//  Created by xLonG on 2019/3/19.
+//  Copyright © 2019年 xLonG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Tools : NSObject
+
+- (void)sayHello;
+@end
---
+++ Classes/Tools.m
@@ -0,0 +1,16 @@
+//
+//  Tools.m
+//  Tools
+//
+//  Created by xLonG on 2019/3/19.
+//  Copyright © 2019年 xLonG. All rights reserved.
+//
+
+#import "Tools.h"
+
+@implementation Tools
+
+- (void)sayHello{
+    NSLog(@"Hello,iOS组件化！！");
+}
+@end
